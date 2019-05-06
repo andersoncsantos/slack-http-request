7e5c154135f0436a2841894967907c6e44c56836
Anderson Santos <andersonc1981@gmail.com>
2019-05-06 10:44:29 -0300

    Update App.java

--- http-request/src/main/java/com/anderson/http_request/App.java
+++ http-request/src/main/java/com/anderson/http_request/App.java
@@ -11,7 +11,7 @@ public class App {
     	HttpResponse<JsonNode>
     		res = Unirest
     				.get("https://slack.com/api/channels.history?"
-    						+ "token=xoxp-318392221105-318942821602-626453242320-bacc497c229b8041d987510a6b7c0048&"
+    						+ "token=your_token_here"
     						+ "channel=C9E03H9L7&"
     						+ "pretty=1")
     				.asJson();
